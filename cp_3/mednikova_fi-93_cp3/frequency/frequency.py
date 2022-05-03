82b214d40e5d61fbf0b52a95e37a71f54f67de85
OleksandraMednikova <olemed-ipt23@lll.kpi.ua>
2022-05-04 02:26:33 +0300

    cipher is added, frequency is defined

---
+++ cp_3/mednikova_fi-93_cp3/frequency/frequency.py
@@ -0,0 +1,29 @@
+from typing import Union, List, Dict, Tuple
+
+#Підрахунок кількості потрібних частинок у тексті
+def frequency_definer(parts: Union[str, List]) -> Dict:
+    frequency = {}
+    for part in parts:
+        frequency[part] = frequency.get(part, 0) + 1
+
+    parts_amount = len(parts)
+    for part, amount in frequency.items():
+        frequency[part] = amount / parts_amount 
+    return frequency
+
+#Сортування даних про частоту
+def frequency_sort(data: Dict) -> List[Tuple[str, int]]:
+    return sorted(data.items(), key=lambda item: item[1], reverse=True)
+
+#Частота біграм, які не перетинаються
+def bigram_frequency_without_intersection(text: str) -> List[Tuple[str, int]]:
+    letters_list = list(text)
+    bigrams = [letters_list[i] + letters_list[i+1] for i in range(0, len(letters_list)-1, 2)]
+    return frequency_sort(frequency_definer(bigrams))
+
+#Частота біграм, які перетинаються
+def bigram_frequency_with_intersection(text: str) -> List[Tuple[str, int]]:
+    letters_list = list(text)
+    bigrams = [letters_list[i] + letters_list[i+1] for i in range(len(letters_list)-1)]
+
+    return frequency_sort(frequency_definer(bigrams))
--- cp_3/mednikova_fi-93_cp3/main.py
+++ cp_3/mednikova_fi-93_cp3/main.py
@@ -1,4 +1,29 @@
-from math_operations.operations import solve_congruence
+from frequency.frequency import bigram_frequency_without_intersection
+from text_getter.text_getter import text_getter
+from cipher.affine_cipher import affine_cipher, affine_decipher
 
-print(solve_congruence((111, 75, 321)))
+fname = 'txt/text_for_theor_freq.txt'
+try:
+    fhandle = open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    print('Default file will be used')
+    fname = 'txt/text_for_theor_freq.txt'
+    fhandle = open(fname)
 
+text_for_theoretical_freq = text_getter(fhandle.read(), {'ъ': 'ь', 'ё': 'е'})
+theoretical_freq = bigram_frequency_without_intersection(text_for_theoretical_freq)[:5]
+fhandle.close()
+
+fname = 'txt/test'
+try:
+    fhandle = open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    print('Default file will be used')
+    fname = 'txt/test'
+    fhandle = open(fname)
+
+ciphertext = text_getter(fhandle.read())
+ciphertext_freq = bigram_frequency_without_intersection(ciphertext)[:5]
+fhandle.close()
